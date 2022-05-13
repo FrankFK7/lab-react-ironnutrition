3c560df4d7ab01c08888c532abc7d20b54cb7463
francisco cano <frankiecano24@gmail.com>
2022-05-13 10:35:14 -0400

    commit 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import foods from './foods.json';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <div>
+          <p> FOOD_NAME_HERE </p>
+          <img src="FOOD_IMAGE_HERE" width={0} />
+        </div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/components/AddFoodForm.js
@@ -0,0 +1,7 @@
+<AddFoodForm food={ {
+  name: "Orange",
+  calories: 85,
+  image: "https://i.imgur.com/abKGOcv.jpg",
+  servings: 1
+  
+}} />
